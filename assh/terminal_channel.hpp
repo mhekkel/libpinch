d6a0c5b782f039fc6bfbaa91f5345ceced025fa3
maarten <maarten@49180b5e-4fd1-4af1-8649-882265d54e9a>
2013-02-22 10:58:02 +0000

    migrate to libassh
    git-svn-id: svn+ssh://s4.hekkelman.net/home/maarten/svnroot/salt/trunk/libassh@176 49180b5e-4fd1-4af1-8649-882265d54e9a

--- assh/terminal_channel.hpp
+++ assh/terminal_channel.hpp
@@ -16,6 +16,22 @@ class terminal_channel : public channel
 					terminal_channel(basic_connection& connection);
 	
 	virtual void	setup(ipacket& in);
+
+	template<typename Handler>
+	void			open_with_pty(uint32 width, uint32 height,
+						const std::string& terminal_type,
+						bool forward_agent, bool forward_x11,
+						Handler&& handler)
+					{
+						m_width = width;
+						m_height = height;
+						m_terminal_type = terminal_type;
+						m_forward_agent = forward_agent;
+						m_forward_x11 = forward_x11;
+						
+						open(std::move(handler));
+					}
+
 	void			open_with_pty(uint32 width, uint32 height,
 						const std::string& terminal_type,
 						bool forward_agent, bool forward_x11);
